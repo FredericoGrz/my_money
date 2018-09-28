782d7c3fcd0a5a8dd9f5a18753094865dc630f86
Frederico Graziani <fredericograziani@Fredericos-MacBook-Pro.local>
2018-09-27 19:51:10 -0700

    Package and webpack configuration

---
+++ frontend/webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    //Entry File
+    entry: './src/index.jsx',
+    //Output Options
+    output: {
+        //Dir of output file
+        path: __dirname + '/public',
+        //Name of output file
+        file: './app.js'
+    },
+    //Server options
+    devServer: {
+        //Port of server
+        port: 8080,
+        //
+        contentBase: './public'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules',
+            jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        new ExtractTextPlugin('app.css')
+    ],
+    module: {
+        loaders: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+            loader: 'file'
+        }]
+    }
+}
