086af49d3f60e648149e1d87bc24897df9a376ee
Frederico Graziani <fredericograziani@Fredericos-MacBook-Pro.local>
2018-09-29 15:59:01 -0700

    Create index.jsx and parts of template

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+    <strong>
+        Copyright &copy; 2018
+        <a href='http://www.facebook.com/frederico.graziani' target='_blank'> Frederico G.</a>
+    </strong>
+    </footer>
+)
